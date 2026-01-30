1467f21bd08ddc560dbc5141172a3aaec9d0f1c3
unknown <https://github.com/jefferson09-dev/project-laravel.git>
2026-01-30 18:47:24 -0300

    projeto em andamento

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\UsersController;
 
 
 
 Route::get('/', function () {
      return view('startbootstrap.index');
 });
-Route::get('/register', function(){
+Route::get('/register', function() {
      return view('startbootstrap.register');
+});
+Route::post('/register', function() {
+     return $_POST;
+     // "usermail":"jeffersonn.rodrigo.09@gmail.com","userpasse":"1234","userpasseconfirma":"1234"
 });
